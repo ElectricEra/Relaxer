c7f6907c1c0975718d0aa36cb6c19aafd6b9a9d5
unknown <nazardishakk@gmail.com>
2017-10-08 00:13:52 +0300

    qweqeqwe

--- src/scripts/components/HeaderFooter/Header.js
+++ src/scripts/components/HeaderFooter/Header.js
@@ -17,18 +17,14 @@ class Header extends React.Component {
 		<nav>
 		    <div className="nav-wrapper">
 		        <Link to='/'><span className="brand-logo"><i className="material-icons">beach_access</i></span></Link>
-		        <ul id="nav-mobile" className="right hide-on-small-only">
+		        <ul id="nav-mobile" className="right hide-on-med-and-down">
 		            <li><Link to='/app'>App</Link></li>
 		            <li><Link to='/host'>Host</Link></li>
 		            <li><SignInComponent /></li>
 		        </ul>
 
-		        <ul id="slide-out" className="side-nav">
-		        <li>
-		        	<div class="user-view">
-						<li className="relaxer-text"><Link to='/'>Relaxer</Link></li>
-				    </div>
-			    </li>
+		        <ul id="slide-out" className="side-nav" onClick={$('.button-collapse').sideNav('hide')}>
+			        <li><Link to='/'>Relaxer</Link></li>
 				    <li><Link to='/app'>App</Link></li>
 		            <li><Link to='/host'>Host</Link></li>
 		            <li><a><SignInComponent /></a></li>
