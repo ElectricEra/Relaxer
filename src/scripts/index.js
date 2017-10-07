0e289e49bd8eaea309d94ced7942149299984c49
Yuriy Pankiv <pankiv97@gmail.com>
2017-10-07 17:36:44 +0300

    card ui updates

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,7 +9,7 @@ import { browserHistory } from 'react-router'
 
 import reducers from './reducers'
 import AppContainer from './components/AppContainer'
-import HostFieldsComponent from './components/HostFieldsComponent/HostFieldsComponent'
+import HostListComponent from './components/CommonComponents/HostListComponent'
 import SignInComponent from './components/SignIn/SignInComponent'
 import UserSearch from './components/User/UserSearch'
 import Header from './components/HeaderFooter/Header'
@@ -26,7 +26,7 @@ ReactDOM.render(
 				<Header />
 				<Route exact path="/" component={AppContainer} />
 				<Route path="/app" component={UserSearch} />
-				<Route path="/host" component={HostFieldsComponent} />
+				<Route path="/host" component={HostListComponent} />
 				<Route path="/sign-in" component={SignInComponent} />
 				<Footer />
 			</div>
