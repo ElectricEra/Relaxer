218b3ac1269005004978ff275fe9d49d647a26ad
Yuriy Pankiv <pankiv97@gmail.com>
2017-10-07 23:49:16 +0300

    updated header

--- src/scripts/components/HeaderFooter/Header.js
+++ src/scripts/components/HeaderFooter/Header.js
@@ -8,6 +8,10 @@ class Header extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+  	$(".button-collapse").sideNav();
+  }
+
   render() {
     return (
 		<nav>
@@ -18,6 +22,18 @@ class Header extends React.Component {
 		            <li><Link to='/host'>Host</Link></li>
 		            <li><SignInComponent /></li>
 		        </ul>
+
+		        <ul id="slide-out" className="side-nav">
+		        <li>
+		        	<div class="user-view">
+						<li className="relaxer-text"><Link to='/'>Relaxer</Link></li>
+				    </div>
+			    </li>
+				    <li><Link to='/app'>App</Link></li>
+		            <li><Link to='/host'>Host</Link></li>
+		            <li><a><SignInComponent /></a></li>
+				</ul>
+				<a href="#" data-activates="slide-out" className="button-collapse"><i className="material-icons">menu</i></a>
 		    </div>
 		</nav>
     )}
