db096a5adefa3b0f46c6236549c7cccb1e058852
Yuriy Pankiv <pankiv97@gmail.com>
2017-10-07 17:41:02 +0300

    form components

---
+++ src/scripts/components/User/UserFormComponent.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import InputComponent from "../CommonComponents/InputComponent";
+import DropdownComponent from "../CommonComponents/DropdownComponent";
+
+class UserFormComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <InputComponent id="price" type="text" labelText="Price" />
+            <DropdownComponent id="amountOfPeople" optionList={[1,2,3,4,5]} />
+          </div>
+        </form>
+      </div>
+    )
+  }
+};
+
+
+export default UserFormComponent;
