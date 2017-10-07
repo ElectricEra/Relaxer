11b4bff73179e715e748a8357f7f4954b066ed33
mkhodan <mykola.khodan@gmail.com>
2017-10-07 16:25:12 +0300

    added host small-card component

---
+++ src/scripts/components/CommonComponents/SmallCardComponent.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+class SmallCardComponent extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="col s12 m6">
+          <div className="card horizontal">
+            <div className="card-image">
+              <img className='card-img' src="http://www.apicius.es/wp-content/uploads/2012/07/IMG-20120714-009211.jpg"/>
+            </div>
+            <div className="card-stacked">
+              <div className="card-content">
+                <h5>Description:</h5>
+                <p>{this.props.description}</p>
+                <h5>Location:</h5>
+                <p>{this.props.location}</p>
+              </div>
+              <div className="card-action">
+                <a href="#">Details</a>
+              </div>
+            </div>
+          </div>
+      </div>
+    )
+  }
+}
+
+export default SmallCardComponent
